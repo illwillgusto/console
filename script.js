81a7a78521942047a8a6fca5269adac27e302853
Will Walton <will@endless3dge.com>
2024-02-05 15:10:23 -0600

    started

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(100);
+console.log('Hello World');
+console.log(20, 'Hello World', true);
+
+console.log('Hello friends');
+
+console.table({name: 'Will', email: 'will@gmail.com'});
+
+console.group('simple');
+
